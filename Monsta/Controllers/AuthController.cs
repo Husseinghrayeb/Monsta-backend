c4bbb8581e2dd243dca1763032cd6ab9f55db2b6
Hussein Ghrayeb <husseinghrayeb97@gmail.com>
2023-01-06 15:54:55 +0200

    Add project files.

---
+++ Monsta/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Monsta_backend.DBContexts;
+using Monsta_backend.JwtFeatures;
+using Monsta_backend.Models;
+using Org.BouncyCastle.Crypto.Generators;
+
+namespace Monsta_backend.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController: ControllerBase
+    {
+        private readonly MyDBContext db;
+        private readonly JwtHandler _jwtHandler;
+        public AuthController(JwtHandler jwtHandler, MyDBContext myDBContext)
+        {
+            db = myDBContext;
+            _jwtHandler = jwtHandler;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            var user = db.Users.FirstOrDefault(user => user.Email == userForAuthentication.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(userForAuthentication.Password, user.Password))
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Wrong username or password" });
+            var signingCredentials = _jwtHandler.GetSigningCredentials();
+            var claims = _jwtHandler.GetClaims(user);
+            var tokenOptions = _jwtHandler.GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+    }
+}
---
+++ Monsta/DBContexts/MyDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Monsta_backend.Entities;
+
+namespace Monsta_backend.DBContexts
+{
+    public class MyDBContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+        }
+    }
+}
